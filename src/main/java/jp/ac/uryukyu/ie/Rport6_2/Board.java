f935de7bd14915096dca318aba38d5f2984fee6b
e175751 <e175751 e175751@ie.u-ryukyu.ac.jp>
2018-01-14 12:06:13 +0900

    first commit

--- src/main/java/jp/ac/uryukyu/ie/Rport6_2/Board.java
+++ src/main/java/jp/ac/uryukyu/ie/Rport6_2/Board.java
@@ -3,18 +3,44 @@ package jp.ac.uryukyu.ie.Rport6_2;
 import java.util.Arrays;
 
 public class Board {
+    
     public static void main(String[] args) {
-        int height;
-        int width;
-        int board_square;
-        board_square = 0;
+        int height, width;
+        int board_square = 0;
         int[][] ban = new int[14][14];
-        for (height = 0; height < 15; height++) {
-            for (width = 0; width < 15; width++) {
+        for (height = 0; height < 14; height++) {
+            for (width = 0; width < 14; width++) {
                 ban[height][width] = board_square;
+                //System.out.print(Arrays.deepToString(ban));
             }
         }
-        System.out.print( Arrays.deepToString(ban));
+        //change_num(ban);
+        ban[0][4] = 7;
+        //System.out.print(Arrays.deepToString(ban));
+        int y = -1;
+        int x = -1;
+
+        for(int i = 0; i<14; i++){
+            y+=1;
+            if(y==14){
+                y=13;
+            }
+            //System.out.print(y);
+            for(int j = 0; j<14; j++){
+
+                x += 1;
+                if(x==14){
+                    x=0;
+                }
+                //System.out.print(x);
+                if(j<13) {
+                    System.out.print(ban[y][x]);
+                }else{System.out.println(ban[y][x]);
+                }
+            }
+        }
+
+
     }
 }
 
--- src/main/java/jp/ac/uryukyu/ie/Rport6_2/Order.java
+++ src/main/java/jp/ac/uryukyu/ie/Rport6_2/Order.java
@@ -1,4 +1,10 @@
 package jp.ac.uryukyu.ie.Rport6_2;
 
 public class Order {
+    private boolean judgment;
+
+
+    public void win(){
+        //勝利判定を行う
+    }
 }
