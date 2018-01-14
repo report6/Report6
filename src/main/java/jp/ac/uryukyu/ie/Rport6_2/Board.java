dd4dfecd62d9f7e8d9715d5245808cb90f9445c7
e175751 <e175751 e175751@ie.u-ryukyu.ac.jp>
2018-01-14 11:43:07 +0900

    first commit

--- src/main/java/jp/ac/uryukyu/ie/Rport6_2/Board.java
+++ src/main/java/jp/ac/uryukyu/ie/Rport6_2/Board.java
@@ -1,4 +1,20 @@
 package jp.ac.uryukyu.ie.Rport6_2;
 
+import java.util.Arrays;
+
 public class Board {
+    public static void main(String[] args) {
+        int height;
+        int width;
+        int board_square;
+        board_square = 0;
+        int[][] ban = new int[14][14];
+        for (height = 0; height < 15; height++) {
+            for (width = 0; width < 15; width++) {
+                ban[height][width] = board_square;
+            }
+        }
+        System.out.print( Arrays.deepToString(ban));
+    }
 }
+
