e09f5cd25214f3f9970ead6968f8d9d743143ea6
e175751 <e175751 e175751@ie.u-ryukyu.ac.jp>
2018-01-14 13:42:10 +0900

    ボードに数字を入力できるようになった

--- src/main/java/jp/ac/uryukyu/ie/Rport6_2/Player.java
+++ src/main/java/jp/ac/uryukyu/ie/Rport6_2/Player.java
@@ -1,6 +1,5 @@
 package jp.ac.uryukyu.ie.Rport6_2;
 
-import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Player {
@@ -8,59 +7,56 @@ public class Player {
     private boolean position;
     private int count;
     private String name;
+    Board ban = new Board();
 
-    public Player(boolean position,int count,String name){
-        this.count = count;
+    public Player(String name) {
         this.name = name;
-        System.out.print("あなたの名前を入力してください");
-        Scanner scan = new Scanner(System.in);
-        name = scan.next();
-    }
-
-    public void main(){
-        Scanner scan = new Scanner(System.in);
-        System.out.printf("%sの番です",name);
+        System.out.printf("%sの番です", name);
         System.out.printf("置く場所を決めてください");
+        Scanner scan = new Scanner(System.in);
         int num = scan.nextInt();
+        int num2 = scan.nextInt();
+        ban.board()[num][num2]=;
+
     }
 
-    public boolean isEmpty(){
+/*
+    public boolean isEmpty() {
         //指定した場所が空かどうか調べる
 
     }
 
-    public boolean getEmpty(){
+    public boolean getEmpty() {
 
     }
 
-    public boolean isMine (){
+    public boolean isMine() {
         //指定した場所に自分の石を置いてあるか調べる
     }
 
-    public boolean getMine () {
+    public boolean getMine() {
 
     }
 
-    public boolean isOpponent (){
+    public boolean isOpponent() {
         //指定した場所に相手の石が置いてあるか調べる
     }
 
-    public boolean getOpponent (){
+    public boolean getOpponent() {
 
     }
 
 
-
-    public void put (){
+    public void put() {
         //指定した場所に石を置く
     }
+    */
 
-    public void win(){
-        System.out.printf("%sの勝利です",name);
+    public void win() {
+        System.out.printf("%sの勝利です", name);
     }
 
-    public void lose(){
-        System.out.printf("%sの負けです",name);
-    }
+    public void lose() { System.out.printf("%sの負けです", name); }
+}
 
 
