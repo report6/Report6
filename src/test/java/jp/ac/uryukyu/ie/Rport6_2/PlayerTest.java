2d781cbe20c59807fda5414c2b24e78f95e698fe
e175751 <e175751 e175751@ie.u-ryukyu.ac.jp>
2018-01-16 00:16:56 +0900

    test

---
+++ src/test/java/jp/ac/uryukyu/ie/Rport6_2/PlayerTest.java
@@ -0,0 +1,23 @@
+package jp.ac.uryukyu.ie.Rport6_2;
+
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    @Test
+
+    public void put(String name) {
+        Board ban = new Board();
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int num2 = scan.nextInt();
+
+
+    }
+
+
+
+}
