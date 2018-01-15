f0c8c25ac8a99dbd786c8406fcb2abb3aae0e0a4
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2018-01-15 10:38:13 +0900

    ボード完成_改良

--- src/main/java/jp/ac/uryukyu/ie/Rport6_2/Board.java
+++ src/main/java/jp/ac/uryukyu/ie/Rport6_2/Board.java
@@ -1,19 +1,24 @@
 package jp.ac.uryukyu.ie.Rport6_2;
 
 import java.util.Arrays;
+import java.util.Stack;
+
 
 public class Board {
     public Board (){//this.board();
     }
     // public static void main(String[] args) {
-    public static int[][] board() {
+    public static String[][] board() {
         int height, width;
-        int board_square = 0;
-        int[][] ban = new int[14][14];
+        String board_square = "e";
+        String[][] ban = new String[14][14];
         for (height = 0; height < 14; height++) {
-            for (width = 0; width < 14; width++)
+            for (width = 0; width < 14; width++) {
                 ban[height][width] = board_square;
+
+            }
         }
+
         //System.out.print(Arrays.deepToString(ban));
         return ban;
     }
@@ -21,30 +26,41 @@ public class Board {
     //change_num(ban);
     //ban[0][4] = "";
     //System.out.print(Arrays.deepToString(ban));
-    public static void boarddisplay(int ban[][]) {
+    public static void boarddisplay(String ban[][]) {
         int y = -1;
         int x = -1;
-
+        String abc[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"};
+        String num[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"};
         for (int i = 0; i < 14; i++) {
+
             y += 1;
             if (y == 14) {
                 y = 13;
             }
             //System.out.print(y);
             for (int j = 0; j < 14; j++) {
-
+                if (i == 0 && j == 0) {
+                    for (int t = 0; t < 14; t++) {
+                        if (t < 13) {
+                            System.out.print(String.format("%2s",num[t]));
+                        } else {
+                            System.out.println(String.format("%2s",num[t]));
+                        }
+                    }
+                }
                 x += 1;
                 if (x == 14) {
                     x = 0;
                 }
                 //System.out.print(x);
                 if (j < 13) {
-                    System.out.print(ban[y][x]);
+                    System.out.print(String.format("%2s",ban[y][x]));
                 } else {
-                    System.out.println(ban[y][x]);
+                    System.out.print(String.format("%2s",ban[y][x]));
+                    System.out.println(String.format("%2s",num[i]));
+
                 }
             }
         }
     }
 }
-
